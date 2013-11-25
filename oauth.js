74e68cb6252c2a884cda63b8d39217eeb20535bf
Michael Herman <hermanmu@gmail.com>
2013-11-24 19:32:41 -0700

    remove oauth

--- oauth.js
+++
@@ -1,9 +0,0 @@
-var ids = {
-  github: {
-    clientID: '4002834d22c3eb20348a',
-    clientSecret: '67d3ada9ce2e3260804113255863a379cdcec36a',
-    callbackURL: "http://127.0.0.1:1337/auth/github/callback"
-  }
-}
-
-module.exports = ids
